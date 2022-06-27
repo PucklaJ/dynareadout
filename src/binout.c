1fff33dd91771693156814310c8b4d7a3b1eeec3
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-06-27 18:50:36 +0200

    Read binout header

--- src/binout.c
+++ src/binout.c
@@ -1,3 +1,57 @@
 #include "binout.h"
+#include "binout_defines.h"
+#include <errno.h>
+#include <string.h>
 
-float add(float a, float b) { return a + b; }
+binout_file binout_open(const char *file_name) {
+  binout_file bin_file;
+  bin_file.error_string = NULL;
+
+  bin_file.file_handle = fopen(file_name, "r");
+  if (!bin_file.file_handle) {
+    bin_file.error_string = strerror(errno);
+    return bin_file;
+  }
+
+  /* Read header */
+  const size_t count =
+      fread(&bin_file.header, 1, sizeof(bin_file.header), bin_file.file_handle);
+  if (count == 0) {
+    bin_file.error_string = strerror(errno);
+    return bin_file;
+  }
+
+  /* Only support Little Endian */
+  if (bin_file.header.endianess == BINOUT_HEADER_BIG_ENDIAN) {
+    bin_file.error_string = "Big Endian is not supported";
+  }
+
+  return bin_file;
+}
+
+void binout_close(binout_file *bin_file) {
+  if (fclose(bin_file->file_handle) != 0) {
+    bin_file->error_string = strerror(errno);
+  }
+}
+
+void binout_print_header(binout_file *bin_file) {
+  printf("Number of Bytes in header: %d\n", bin_file->header.header_size);
+  printf("Number of Bytes used in record LENGTH fields: %d\n",
+         bin_file->header.record_length_field_size);
+  printf("Number of Bytes used in record OFFSET fields: %d\n",
+         bin_file->header.record_offset_field_size);
+  printf("Number of Bytes used in record COMMAND fields: %d\n",
+         bin_file->header.record_command_field_size);
+  printf("Number of Bytes used in record TYPEID fields: %d\n",
+         bin_file->header.record_typeid_field_size);
+  printf("Endianess: %s\n",
+         bin_file->header.endianess == BINOUT_HEADER_BIG_ENDIAN
+             ? "Big Endian"
+             : (bin_file->header.endianess == BINOUT_HEADER_LITTLE_ENDIAN
+                    ? "Little Endian"
+                    : "Unknown"));
+  printf("Floating Point Format: %s\n",
+         bin_file->header.float_format == BINOUT_HEADER_FLOAT_IEEE ? "IEEE"
+                                                                   : "Unknown");
+}
--- src/binout.h
+++ src/binout.h
@@ -1,3 +1,34 @@
-#pragma once
+#ifndef BINOUT_H
+#define BINOUT_H
+#include <stdint.h>
+#include <stdio.h>
 
-float add(float a, float b);
+typedef struct {
+  uint8_t header_size;
+  uint8_t record_length_field_size;
+  uint8_t record_offset_field_size;
+  uint8_t record_command_field_size;
+  uint8_t record_typeid_field_size;
+  uint8_t endianess;
+  uint8_t float_format;
+  uint8_t _unused;
+} binout_header;
+
+typedef struct {
+  binout_header header;
+
+  FILE *file_handle;
+  const char *error_string;
+} binout_file;
+
+/* ----- Public functions ------ */
+
+binout_file binout_open(const char *file_name);
+void binout_close(binout_file *bin_file);
+void binout_print_header(binout_file *bin_file);
+
+/* ----------------------------- */
+
+/* ----- Private functions ----- */
+/* ----------------------------- */
+#endif
