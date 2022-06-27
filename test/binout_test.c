9bf838b939ce143c5f03bd8eeb2699853abedfbd
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-06-28 00:35:56 +0200

    Add an argument to the test binary

--- test/binout_test.c
+++ test/binout_test.c
@@ -1,7 +1,14 @@
 #include <binout.h>
 
 int main(int args, char *argv[]) {
-  binout_file bin_file = binout_open("test_data/binout0000");
+  if (args < 2) {
+    fprintf(stderr, "Invalid Arguments\n");
+    return 1;
+  }
+
+  const char *binout_file_name = argv[1];
+
+  binout_file bin_file = binout_open(binout_file_name);
   if (bin_file.error_string) {
     fprintf(stderr, "Failed to open binout: %s\n", bin_file.error_string);
     return 1;
