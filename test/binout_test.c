5e94c1b015294b28624e50d748382cac10bcf1ae
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-06-27 19:48:31 +0200

    Read all binout records

--- test/binout_test.c
+++ test/binout_test.c
@@ -3,11 +3,12 @@
 int main(int args, char *argv[]) {
   binout_file bin_file = binout_open("test_data/binout0000");
   if (bin_file.error_string) {
-    fprintf(stderr, "Failed to open binout: %s", bin_file.error_string);
+    fprintf(stderr, "Failed to open binout: %s\n", bin_file.error_string);
     return 1;
   }
 
   binout_print_header(&bin_file);
+  binout_print_records(&bin_file);
 
   binout_close(&bin_file);
 
