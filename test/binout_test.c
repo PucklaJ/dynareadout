c4e84845c456b21e28d639a941d84db68bbba447
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-06-28 00:32:16 +0200

    Implement read functions

--- test/binout_test.c
+++ test/binout_test.c
@@ -10,6 +10,38 @@ int main(int args, char *argv[]) {
   binout_print_header(&bin_file);
   binout_print_records(&bin_file);
 
+  size_t legend_size;
+  int8_t *legend =
+      binout_read_int8_t(&bin_file, "/nodout/metadata", "legend", &legend_size);
+  if (!legend) {
+    fprintf(stderr, "Failed to read legend: %s\n", bin_file.error_string);
+    binout_close(&bin_file);
+    return 1;
+  }
+
+  legend = realloc(legend, legend_size + 1);
+  legend[legend_size] = '\0';
+  printf("nodout legend: %s\n", legend);
+  free(legend);
+
+  size_t node_ids_size;
+  int32_t *node_ids =
+      binout_read_int32_t(&bin_file, "/rcforc/metadata", "ids", &node_ids_size);
+  if (!node_ids) {
+    fprintf(stderr, "Failed to read node ids: %s\n", bin_file.error_string);
+    binout_close(&bin_file);
+    return 1;
+  }
+
+  printf("---- Node IDs: %d -----\n", node_ids_size);
+  uint64_t i = 0;
+  while (i < node_ids_size) {
+    printf("%d, ", node_ids[i]);
+
+    i++;
+  }
+  printf("\n--------------------------\n");
+
   binout_close(&bin_file);
 
   return 0;
