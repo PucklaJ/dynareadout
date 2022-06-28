6d95c8aef7f28b0855db54f26fd10bb2ca1d0871
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-06-28 20:26:07 +0200

    Add build_test option

--- xmake.lua
+++ xmake.lua
@@ -1,18 +1,25 @@
 set_project("dynareadout")
+
+option("build_test")
+    set_default(false)
+    set_showmenu(true)
+option_end()
+
 add_rules("mode.debug", "mode.release")
-
-add_requires("doctest")
-
 target("binout")
     set_kind("static")
     set_languages("ansi")
     add_files("src/*.c")
     add_headerfiles("src/*.h")
+target_end()
 
-target("binout_test")
-    set_kind("binary")
-    set_languages("cxx11")
-    add_deps("binout")
-    add_packages("doctest")
-    add_includedirs("src")
-    add_files("test/binout_test.cpp")
+if get_config("build_test") then
+    add_requires("doctest")
+    target("binout_test")
+        set_kind("binary")
+        set_languages("cxx11")
+        add_deps("binout")
+        add_packages("doctest")
+        add_includedirs("src")
+        add_files("test/binout_test.cpp")
+end
