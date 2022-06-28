53466088993b39d07716bae9b662efd23c71810d
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-06-28 11:41:25 +0200

    Implement path_parse

--- src/path.h
+++ src/path.h
@@ -1,5 +1,6 @@
 #ifndef PATH_H
 #define PATH_H
+#include <stdlib.h>
 
 #define PATH_SEP '/'
 
@@ -10,6 +11,9 @@ extern "C" {
 char *path_join(char *path, const char *element);
 int path_is_abs(const char *path);
 char *path_main(const char *path);
+char *path_parse(char *path);
+
+char *delete_substr(char *path, size_t start, size_t end);
 
 #ifdef __cplusplus
 }
