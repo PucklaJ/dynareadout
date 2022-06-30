f7184caf770e56767a181f1a3250ab07c564472d
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-06-30 22:46:27 +0200

    Complete binout0000 C++ test

--- src/cpp/vector.hpp
+++ src/cpp/vector.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <cstdlib>
+#include <cstring>
+#include <iostream>
 #include <stdexcept>
+#include <type_traits>
 
 namespace dro {
 
@@ -10,12 +13,15 @@ public:
   Vector(Vector<T> &&rhs);
   ~Vector();
 
-  T &operator[](size_t index);
-  const T &operator[](size_t index) const;
+  inline T &operator[](size_t index);
+  inline const T &operator[](size_t index) const;
   Vector<T> &operator=(Vector<T> &&rhs);
 
   T *data() { return m_data; }
   const T *data() const { return m_data; }
+  size_t size() const { return m_size; }
+
+  std::string str() const;
 
 private:
   T *m_data;
@@ -60,9 +66,52 @@ template <typename T> Vector<T> &Vector<T>::operator=(Vector<T> &&rhs) {
   return *this;
 }
 
+template <typename T> std::string Vector<T>::str() const {
+  static_assert(std::is_same_v<T, char> || std::is_same_v<T, int8_t> ||
+                std::is_same_v<T, uint8_t>);
+
+  std::string str(reinterpret_cast<const char *>(m_data), m_size);
+  return str;
+}
+
 typedef Vector<char> String;
 
 } // namespace dro
 
-bool operator==(const dro::String &str1, const char *str2);
-std::ostream &operator<<(std::ostream &stream, const dro::String &str);
+template <typename T>
+bool operator==(const dro::Vector<T> &str1, const char *str2) {
+  static_assert(std::is_same_v<T, char> || std::is_same_v<T, int8_t> ||
+                std::is_same_v<T, uint8_t>);
+  if (str1.data()[str1.size() - 1] == '\0') {
+    return strcmp(reinterpret_cast<const char *>(str1.data()), str2) == 0;
+  }
+
+  if (strlen(str2) != str1.size()) {
+    return false;
+  }
+
+  for (size_t i = 0; i < str1.size(); i++) {
+    if (static_cast<char>(str1[i]) != str2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+template <typename T>
+inline bool operator==(const char *str2, const dro::Vector<T> &str1) {
+  return str1 == str2;
+}
+
+template <typename T>
+inline std::ostream &operator<<(std::ostream &stream,
+                                const dro::Vector<T> &str) {
+  static_assert(std::is_same_v<T, char> || std::is_same_v<T, int8_t> ||
+                std::is_same_v<T, uint8_t>);
+
+  for (size_t i = 0; i < str.size(); i++) {
+    stream << static_cast<char>(str[i]);
+  }
+  return stream;
+}
