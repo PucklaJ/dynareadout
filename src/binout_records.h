c682299cb4f3b1333adce8fa2ab8bc497e95409a
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-07-02 20:15:01 +0200

    Add comments to binout_records.h

--- src/binout_records.h
+++ src/binout_records.h
@@ -4,17 +4,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/* Represents a data record of a binout file*/
 typedef struct {
-  path_t path;
-  size_t file_pos;
+  path_t path;     /* The path inside the binout file*/
+  size_t file_pos; /* At which file position the data segment of the record can
+                      be found*/
 } binout_record_data;
 
+/* Holds all the data records of a variable*/
 typedef struct {
-  uint64_t type_id;
-  char *name;
-  uint64_t data_length;
-  binout_record_data *records;
-  size_t records_size;
+  uint64_t type_id;     /* The type id of the variable*/
+  char *name;           /* The name of the variable*/
+  uint64_t data_length; /* The length of a data segment of a record in bytes*/
+  binout_record_data *records; /* An array holding all records*/
+  size_t records_size;         /* The number of elements in records*/
 } binout_record_data_pointer;
 
 #endif
