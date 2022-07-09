f24ec09fc07f077b2bb5535ab3bea70a2212053e
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2022-07-09 14:44:26 +0200

    Add comments to d3plot.hpp

--- src/cpp/d3plot.hpp
+++ src/cpp/d3plot.hpp
@@ -32,6 +32,7 @@
 
 namespace dro {
 
+// This holds all data needed to read d3plot files
 class D3plot {
 public:
   class Exception : public std::exception {
@@ -44,30 +45,53 @@ public:
     const String m_error_str;
   };
 
+  // Open a d3plot file family by giving the root file name
+  // Example: d3plot of d3plot01, d3plot02, d3plot03, etc.
   D3plot(const std::filesystem::path &root_file_name);
   ~D3plot() noexcept;
 
+  // Read all ids of the nodes
   Array<d3_word> read_node_ids();
+  // Read all ids of the solid elements
   Array<d3_word> read_solid_element_ids();
+  // Read all ids of the beam elements
   Array<d3_word> read_beam_element_ids();
+  // Read all ids of the shell elements
   Array<d3_word> read_shell_element_ids();
+  // Read all ids of the solid shell elements
   Array<d3_word> read_solid_shell_element_ids();
+  // Read all ids of the solid, beam, shell and solid shell elements
   Array<d3_word> read_all_element_ids();
 
+  // Read the node coordinates of all nodes of a given state (time step)
   Array<dVec3> read_node_coordinates(size_t state);
+  // Read the node velocity of all nodes of a given state (time step)
   Array<dVec3> read_node_velocity(size_t state);
+  // Read the node acceleration of all nodes of a given state (time step)
   Array<dVec3> read_node_acceleration(size_t state);
+  // Read the time of a given state (time step) in milliseconds
   double read_time(size_t state);
 
+  // Returns the node connectivity + material number of all 8 node solid
+  // elements
   Array<d3plot_solid> read_solid_elements();
+  // Returns the node connectivity + material number of all 8 node thick shell
+  // elements
   Array<d3plot_thick_shell> read_thick_shell_elements();
+  // Returns the node connectivity + orientation node + material number of all
+  // beam elements
   Array<d3plot_beam> read_beam_elements();
+  // Returns the node connectivity + material number of all shell
+  // elements
   Array<d3plot_shell> read_shell_elements();
+  // Returns a string holding the Title of the d3plot file
   String read_title();
 
+  // Returns the number of states (time steps)
   inline size_t num_time_steps() const { return m_handle.num_states; }
 
 private:
+  // The underlying C handle of the d3plot file
   d3plot_file m_handle;
 };
 
