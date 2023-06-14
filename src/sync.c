ac8893be58fcda47e6d2c7c89ce8db7880f51691
PucklaJ <jonaas.pucher000000@gmail.com>
2023-06-14 19:02:36 +0200

    Implement mutex for windows

--- src/sync.c
+++ src/sync.c
@@ -1,7 +1,77 @@
 #include "sync.h"
+#include "errno.h"
 #include "profiling.h"
 
 #ifdef _WIN32
+sync_t sync_create() {
+  BEGIN_PROFILE_FUNC();
+
+  sync_t snc = (sync_t)CreateMutexW(NULL, FALSE, NULL);
+
+  END_PROFILE_FUNC();
+  return snc;
+}
+
+int sync_lock(sync_t *snc) {
+  BEGIN_PROFILE_FUNC();
+
+  const DWORD win_rv = WaitForSingleObject(*((HANDLE *)snc), INFINITE);
+
+  int rv;
+  switch (win_rv) {
+  case WAIT_FAILED:
+  case WAIT_TIMEOUT:
+    rv = EINVAL;
+    break;
+  default:
+    rv = 0;
+    break;
+  }
+
+  END_PROFILE_FUNC();
+  return rv;
+}
+
+int sync_trylock(sync_t *snc) {
+  BEGIN_PROFILE_FUNC();
+
+  const DWORD win_rv = WaitForSingleObject(*((HANDLE *)snc), 0);
+
+  int rv;
+  switch (win_rv) {
+  case WAIT_TIMEOUT:
+    rv = EBUSY;
+    break;
+  case WAIT_FAILED:
+    rv = EINVAL;
+    break;
+  default:
+    rv = 0;
+    break;
+  }
+
+  END_PROFILE_FUNC();
+  return rv;
+}
+
+int sync_unlock(sync_t *snc) {
+  BEGIN_PROFILE_FUNC();
+
+  const BOOL win_rv = ReleaseMutex(*((HANDLE *)snc));
+  const int rv = win_rv != 0 ? 0 : EINVAL;
+
+  END_PROFILE_FUNC();
+  return rv;
+}
+
+void sync_destroy(sync_t *snc) {
+  BEGIN_PROFILE_FUNC();
+
+  CloseHandle(*((HANDLE *)snc));
+
+  END_PROFILE_FUNC();
+}
+
 #else
 sync_t sync_create() {
   BEGIN_PROFILE_FUNC();
