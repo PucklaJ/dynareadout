0544890b77a9519d81a66a57d188cc26b70fdbdf
PucklaJ <jonaas.pucher000000@gmail.com>
2024-02-16 12:48:51 +0100

    Fix abi-suffix.py by not removing dots

--- src/python/abi-suffix.py
+++ src/python/abi-suffix.py
@@ -1,9 +1,2 @@
 import sysconfig
-import platform
-abi_tag = sysconfig.get_config_var('EXT_SUFFIX').replace('.', '') if sysconfig.get_config_var('EXT_SUFFIX') else None
-if platform.system() == 'Windows':
-    abi_tag += '.dll'
-else:
-    abi_tag += '.so'
-abi_tag = '.' + abi_tag
-print(abi_tag)
+print(sysconfig.get_config_var('EXT_SUFFIX'))
